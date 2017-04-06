44b193b68af50b79fa0b856a7fdb9e1aa2165b84
Erik Ziegler <erik.sweed@gmail.com>
2017-04-06 12:03:08 +0200

    Fix Babel settings and npm dev dependencies. Fix extra closing bracket"

--- src/internal/storedColorPixelDataToCanvasImageData.js
+++ src/internal/storedColorPixelDataToCanvasImageData.js
@@ -1,38 +1,36 @@
 /**
  * This module contains a function to convert stored pixel values to display pixel values using a LUT
  */
-export function storedColorPixelDataToCanvasImageData(image, lut, canvasImageDataData)
-    {
-        var minPixelValue = image.minPixelValue;
-        var canvasImageDataIndex = 0;
-        var storedPixelDataIndex = 0;
-        var numPixels = image.width * image.height * 4;
-        var storedPixelData = image.getPixelData();
-        var localLut = lut;
-        var localCanvasImageDataData = canvasImageDataData;
+export function storedColorPixelDataToCanvasImageData(image, lut, canvasImageDataData) {
+    var minPixelValue = image.minPixelValue;
+    var canvasImageDataIndex = 0;
+    var storedPixelDataIndex = 0;
+    var numPixels = image.width * image.height * 4;
+    var storedPixelData = image.getPixelData();
+    var localLut = lut;
+    var localCanvasImageDataData = canvasImageDataData;
 
-        // Wrap this intensive loop in an IIFE to prevent de-optimization if
-        // the image Object has members added to it.
-        (function () {
-            // NOTE: As of Nov 2014, most javascript engines have lower performance when indexing negative indexes.
-            // We have a special code path for this case that improves performance.  Thanks to @jpambrun for this enhancement
-            if(minPixelValue < 0){
-                while(storedPixelDataIndex < numPixels) {
-                    localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++] + (-minPixelValue)]; // red
-                    localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++] + (-minPixelValue)]; // green
-                    localCanvasImageDataData[canvasImageDataIndex] = localLut[storedPixelData[storedPixelDataIndex] + (-minPixelValue)]; // blue
-                    storedPixelDataIndex+=2;
-                    canvasImageDataIndex+=2;
-                }
-            }else{
-                while(storedPixelDataIndex < numPixels) {
-                    localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++]]; // red
-                    localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++]]; // green
-                    localCanvasImageDataData[canvasImageDataIndex] = localLut[storedPixelData[storedPixelDataIndex]]; // blue
-                    storedPixelDataIndex+=2;
-                    canvasImageDataIndex+=2;
-                }
+    // Wrap this intensive loop in an IIFE to prevent de-optimization if
+    // the image Object has members added to it.
+    (function () {
+        // NOTE: As of Nov 2014, most javascript engines have lower performance when indexing negative indexes.
+        // We have a special code path for this case that improves performance.  Thanks to @jpambrun for this enhancement
+        if(minPixelValue < 0){
+            while(storedPixelDataIndex < numPixels) {
+                localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++] + (-minPixelValue)]; // red
+                localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++] + (-minPixelValue)]; // green
+                localCanvasImageDataData[canvasImageDataIndex] = localLut[storedPixelData[storedPixelDataIndex] + (-minPixelValue)]; // blue
+                storedPixelDataIndex+=2;
+                canvasImageDataIndex+=2;
             }
-        })();
-    }
+        }else{
+            while(storedPixelDataIndex < numPixels) {
+                localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++]]; // red
+                localCanvasImageDataData[canvasImageDataIndex++] = localLut[storedPixelData[storedPixelDataIndex++]]; // green
+                localCanvasImageDataData[canvasImageDataIndex] = localLut[storedPixelData[storedPixelDataIndex]]; // blue
+                storedPixelDataIndex+=2;
+                canvasImageDataIndex+=2;
+            }
+        }
+    })();
 }
